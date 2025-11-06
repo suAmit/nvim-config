78159b56f56d6aefa4de8a2cad007d3841b060bb
suAmit <147977973+suAmit@users.noreply.github.com>
2025-11-06 19:21:12 +0530

    fix: incline.nvim hint icon don't show properly

--- lua/plugins/ui/incline.lua
+++ lua/plugins/ui/incline.lua
@@ -29,7 +29,7 @@ return {
           return labels
         end
         local function get_diagnostic_label()
-          local icons = { error = "", warn = "", info = "", hint = "" }
+          local icons = { error = " ", warn = " ", hint = "󰠠 ", info = " " }
           local label = {}
 
           for severity, icon in pairs(icons) do
